8bdc5bb0b51593fdb37d5794241177886a5653e3
刘光强 <liuguangqiang@qianbaocard.com>
2018-09-17 11:19:48 +0800

    Initial Commit

---
+++ NotificationExtensionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationExtensionDemo
+//
+//  Created by 刘光强 on 2018/9/17.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationExtensionDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NotificationExtensionDemo
+//
+//  Created by 刘光强 on 2018/9/17.
+//  Copyright © 2018年 quangqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
